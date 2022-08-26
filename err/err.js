f4e6bf0d8cd97cc4bbcd29c64e001538cecd00d8
EL KARCHI Jad <Jad.Elkarchi@bordeaux-inp.fr>
2022-08-26 16:19:05 +0200

    added animation and image animation, error page terminated

--- err/err.js
+++ err/err.js
@@ -1,21 +1,21 @@
 const Background = document.querySelector('.error-background');
 const HEIGHT = 390; // pixels
 const WIDTH = 590; // pixels
+const OFFSET = 7; // pixels
 const SHAPES = [
     {
         type: 'rectangle',
-        quantity: 30
+        quantity: 10
     },
     {
         type: 'triangle',
-        quantity: 40
+        quantity: 20
     },
     {
         type: 'circle',
-        quantity: 40
+        quantity: 20
     }
 ];
-let startPoint = false;
 
 
 document.querySelector('.loader-wrapper').classList.add('isActive');
@@ -25,19 +25,20 @@ window.onload = () => {
     document.querySelector('.error-container').classList.add('isVisible');
     setTimeout(() =>{
         document.querySelector('.error-image').classList.add('isVisible');
-        startPoint = true;
     }, 1200);
 };
 
 
 class mesh{
-    constructor(type = '', size = 5, position = {x: 0, y: 0}, velocity = {x: 0, y:0}){
+    constructor(type = '', size = 5, position = {x: 0, y: 0}, velocity = {x: 0, y:0}, angle = 0, angularVelocity = { x: 0, y: 0}){
 
         this.DOMelement;
         this.position = position;
         this.height = size;
         this.width = size;
         this.velocity = velocity;
+        this.angle = angle;
+        this.angularVelocity = angularVelocity;
 
         let div; 
 
@@ -83,8 +84,59 @@ class mesh{
         this.DOMelement.style.top = `${position.y}px`;
     }
 
+    setAngle(angle){
+        this.angle = angle;
+        this.DOMelement.style.transform = `rotate(${angle}deg)`;
+    }
+
     setVelocity(velocity){
         this.velocity = velocity;
+    }
+
+    setAngularVelocity(angularVelocity){
+        this.angularVelocity = angularVelocity;
+    }
+
+    detectCollision(){
+        if (this.position.y - this.height < - OFFSET){
+            return 'up';
+        } else if (this.position.y + this.height > HEIGHT + OFFSET){
+            return 'down';
+        } else if (this.position.x - this.width < - OFFSET){
+            return 'left';
+        } else if (this.position.x + this.width > WIDTH + OFFSET){
+            return 'right';
+        } else {
+            return '';
+        }
+    }
+    
+    update(){
+        
+        switch(this.detectCollision()){ // invert velocity at borders
+            case 'up':
+                this.velocity.y = -this.velocity.y;
+                break;
+            case 'down':
+                this.velocity.y = -this.velocity.y;
+                break;
+            case 'left':
+                this.velocity.x = -this.velocity.x;
+                break;
+            case 'right':
+                this.velocity.x = -this.velocity.x;
+                break;
+            default:
+                break;
+        }
+
+        this.setPosition({
+            x: this.position.x + this.velocity.x,
+            y: this.position.y + this.velocity.y
+        });
+
+        if ( this.DOMelement.className === 'circle') return;
+        this.setAngle(this.angle + this.angularVelocity);
     }
 
 }
@@ -92,6 +144,8 @@ class mesh{
 function createShapes(type, quantity, acc){
     let randomShape = null;
     let randomSize = 0;
+    let randomVelocity = {x: 0, y: 0};
+    let randomAngularVelocity = {x: 0, y: 0};
     let randomPosition = {x: 0, y: 0}; //default for random values
 
     for (let i = 0; i < quantity; i++){
@@ -99,9 +153,19 @@ function createShapes(type, quantity, acc){
         randomPosition = {
             x: Math.floor(Math.random()*WIDTH),
             y: Math.floor(Math.random()*HEIGHT)
-        }
+        };
+
+        randomVelocity = {
+            x: (Math.floor(Math.random()*100)%2 === 0) ? -1 : 1 ,
+            y:  (Math.floor(Math.random()*100)%2 === 0) ? -1 : 1
+        };
+        
+        randomAngularVelocity = (Math.floor(Math.random()*100)%2 === 0) ? -Math.floor(Math.random()*5) - 1 : Math.floor(Math.random()*5) + 1;
+
         randomShape = new mesh(type, randomSize);
         randomShape.setPosition(randomPosition);
+        randomShape.setVelocity(randomVelocity);
+        randomShape.setAngularVelocity(randomAngularVelocity);
 
         acc.push(randomShape);
     }
@@ -126,10 +190,17 @@ function DisplaySetOfShapes( setOfShapes ){
     });
 }
 
+
 DisplaySetOfShapes(setOfShapes);
 
 function animate(){
     requestAnimationFrame(animate);
+
+    setOfShapes.forEach((shape) => {
+        shape.update();
+    });
+
 }
+
 
 animate();
