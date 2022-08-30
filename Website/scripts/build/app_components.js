092960504c0b232ce93d956fb2f5a50c0c01420f
EL KARCHI Jad <Jad.Elkarchi@bordeaux-inp.fr>
2022-08-30 16:33:14 +0200

    added sea icons, fixed animations in index.html fixed loader and scroll onload

--- Website/scripts/build/app_components.js
+++ Website/scripts/build/app_components.js
@@ -1,2 +1,30 @@
 "use strict";
-const variable = 1;
+window.addEventListener('scroll', () => {
+    manageFade(window.scrollY);
+});
+function manageFade(scroll) {
+    if (scroll > 560) {
+        document.querySelector('.resume-programing-languages')?.classList.add('isVisible');
+        document.querySelector('.resume-education')?.classList.add('isVisible');
+        document.querySelector('.return-overview')?.classList.add('isVisible');
+    }
+    if (scroll > 1050) {
+        document.querySelector('.resume-expertise')?.classList.add('isVisible');
+        document.querySelector('.resume-softwares-tools')?.classList.add('isVisible');
+    }
+    if (scroll > 1500) {
+        document.querySelector('.resume-languages')?.classList.add('isVisible');
+        document.querySelector('.resume-hobbies')?.classList.add('isVisible');
+        document.querySelector('.widget-art')?.classList.add('isVisible');
+        document.querySelector('.resume-achievements')?.classList.add('isVisible');
+    }
+    if (scroll > 1600) {
+        document.querySelector('.resume-main-goals')?.classList.add('isVisible');
+    }
+    if (scroll > 2200) {
+        document.querySelector('.resume-brief-paragraph')?.classList.add('isVisible');
+    }
+    if (scroll > 2360) {
+        document.querySelector('.made-with-love')?.classList.add('isVisible');
+    }
+}
